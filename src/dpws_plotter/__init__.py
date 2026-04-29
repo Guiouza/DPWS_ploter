eb18bd48bc083f9af0eab8cd5f90354658c67dbf
Guilherme Meneghin de Souza <guilherme.meneghinsouza@gmail.com>
2026-04-29 00:15:31 -0300

    refactor: Change lybrary name and create stub file Changed the name of the lybrary so the pyip see a diferent project now BREAKING CHAGE

--- src/dpws_plotter/__init__.py
+++ src/dpws_plotter/__init__.py
@@ -1,23 +1,21 @@
-""" Porcelain funcitons for ploting tools
+""" Porcelain functions for plotting tools
 author: Guilherme Meneghin de Souza (guilherme.meneghinsouza@gmail.com)
 """
 
-from src.dpws_ploter import *
+import dpws_plotter.linear_system as ls
 
-def plot(
-        A: Union[NumArray|str], b: Union[NumArray|str],
-        A2: Union[None, NumArray|str]=None, b2: Union[NumArray|str]=None):
+def plot(A, b, A2=None, b2=None):
     # Create Linear Systems
-    F1 = Linear2DSystem(A, b)
+    F1 = ls.Linear2DSystem(A, b)
     print('F1(x,y) = ')
-    F1.show()
+    F1.print()
     F2 = None
     if A2 is not None:
         if b2 is not None:
             b2 = '[0, 0]'
-        F2 = Linear2DSystem(A2, b2)
+        F2 = ls.Linear2DSystem(A2, b2)
         print('F2(x,y) = ')
-        F2.show()
+        F2.print()
     
     f1_range = (-10, 10)
     f2_range = (-10, 10)
---
+++ src/dpws_plotter/linear_system.pyi
@@ -0,0 +1,39 @@
+from numpy.typing import NDArray
+from sympy import MutableDenseMatrix
+from typing import List, Tuple, Union, overload
+
+import numpy as np
+
+
+type Num_NDArray = Union[
+    NDArray[np.number],
+    Tuple[Num_NDArray, Num_NDArray],
+    List[Num_NDArray],
+]
+
+class Linear2DSystem:
+    def __init__(self, A: Num_NDArray|str, b: Num_NDArray|str, symbols: str='x y'):
+        self.A: np.ndarray = None
+        self.b: np.ndarray = None
+        self.sym_A: MutableDenseMatrix = None
+        self.sym_b: MutableDenseMatrix = None
+        self.sym_X: MutableDenseMatrix = None
+
+    def print(self) -> None: ...
+    def symrepr(self) -> MutableDenseMatrix: ...
+    def eval(self, xy: Num_NDArray) -> Num_NDArray: ...
+    def plot(self,
+        x_range: Num_NDArray = (-10,10),
+        y_range: Num_NDArray = (-10,10),
+        n_grid: int = 25, density: float = 1): ...
+
+    def __repr__(self) -> str: ...
+    def __str__(self) -> str: ...
+    @overload
+    def __call__(self, X: Num_NDArray) -> Num_NDArray: ...
+    @overload
+    def __call__(self, X: MutableDenseMatrix) -> MutableDenseMatrix: ...
+    def __add__(self, other: MutableDenseMatrix) -> MutableDenseMatrix: ...
+    def __sub__(self, other: MutableDenseMatrix) -> MutableDenseMatrix: ...
+    def __mul__(self, other: MutableDenseMatrix) -> MutableDenseMatrix: ...
+    def __truediv__(self, other: MutableDenseMatrix) -> MutableDenseMatrix: ...
